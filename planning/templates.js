b4038ef16a9a30f8b96adaafb349001782ec7f9f
Julien Michel <julienmichel.pro@gmail.com>
2016-02-24 12:22:29 +0100

    switch height

--- planning/templates.js
+++ planning/templates.js
@@ -85,7 +85,7 @@ angular.module('90Tech.planning').run(['$templateCache', function($templateCache
 
   $templateCache.put('planning/directives/planning-week-line/planning-week-line.html',
     "<div class=\"week-line\" ng-class=\"'week-' + line.week \" style=\"padding-top:20px\">\n" +
-    "    <div class=\"multiple-days-events\" style=\"height:60%; overflow-y: none;\">\n" +
+    "    <div class=\"multiple-days-events\" style=\"height:40%;\">\n" +
     "        <div class=\"week-event-line\" ng-repeat=\"l in line.lines\" style=\"width:100%; position:relative\"\n" +
     "             ng-style=\"{'height': line.lh}\">\n" +
     "            <div ng-repeat=\"event in l\"\n" +
@@ -96,7 +96,7 @@ angular.module('90Tech.planning').run(['$templateCache', function($templateCache
     "            </div>\n" +
     "        </div>\n" +
     "    </div>\n" +
-    "    <div class=\"single-day-events\" style=\"height:40%; position:relative;\">\n" +
+    "    <div class=\"single-day-events\" style=\"height:60%; position:relative;\">\n" +
     "        <div ng-repeat=\"ev in line.oneDayEvents\"\n" +
     "             ng-style=\"{'background-color': ev.color,'left': ev.style.left, 'height': ev.style.height , 'top': ev.style.top, 'width': ev.style.width}\"\n" +
     "             style=\"position:absolute;border: 1px solid black;\"\n" +
