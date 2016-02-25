312f540c163543c8b44cb29877c3887189456a5a
Julien Michel <julienmichel.pro@gmail.com>
2016-02-25 16:37:50 +0100

    format planning.js

--- planning/directives/planning/planning.js
+++ planning/directives/planning/planning.js
@@ -12,105 +12,6 @@
 
     var self = this
 
-    function split (event) {
-      // Event starts and ends the same day
-      event = angular.copy(event)
-
-      var start = moment(event.start).hour(self._dayStart.h).minute(self._dayStart.m).second(0)
-      var stop = moment(event.end).hour(self._dayEnd.h).minute(self._dayEnd.m).second(59)
-      if (event.start.dayOfYear() === event.end.dayOfYear()) {
-        // reset the boundaries if they exceed the one fixed.
-        // And trim events that are entirely out of the boundaries
-        if (event.start.isBefore(start)) {
-          if (event.end.isBefore(start)) {
-            return []
-          }
-          event.start = start
-        }
-        if (event.end.isAfter(stop)) {
-          if (event.start.isAfter(stop)) {
-            return []
-          }
-          event.end = stop
-        }
-        return [ event ]
-      }
-      // Event is on several days.
-      // Build a first event that ends at the end of the first day
-      var first_event = angular.copy(event)
-      first_event.end = moment(event.start).hour(self._dayEnd.h).minute(self._dayEnd.m).second(59)
-      if (event.start.isBefore(start)) {
-        first_event.start = start
-      }
-
-      var startNextDay = moment(event.start).add(1, 'day').hour(self._dayStart.h).minute(self._dayStart.m)
-      var endThisDay = moment(event.start).hour(self._dayEnd.h).minute(self._dayEnd.m)
-      if (event.end.isBefore(startNextDay)) {
-        if (first_event.start.isAfter(endThisDay)) {
-          return []
-        }
-        // Event finishes before start hour next day. No need to create another one
-        return [ first_event ]
-      }
-      first_event.continuedAfter = true
-
-      // Build a second event that starts at the beginning of the following days.
-      // This event may end several days later. Recursion will handle it
-      var second_event = angular.copy(event)
-      second_event.start = moment(event.start).add(1, 'day').hour(self._dayStart.h).minute(self._dayStart.m)
-      second_event.continuedBefore = true
-
-      if (event.start.isAfter(endThisDay)) {
-        // If the first event starts after curfew, don't add it
-        return split(second_event)
-      }
-
-      return [ first_event ].concat(split(second_event))
-    }
-
-    function filter (events) {
-      return _.filter(events, function (e) {
-        var start, stop
-        if (self.mode === 'week') {
-          start = moment(self.position).weekday(0).hour(self._dayStart.h).minute(self._dayStart.m).second(0)
-          stop = moment(self.position).weekday(7).hour(self._dayEnd.h).minute(self._dayEnd.m).second(59)
-        } else if (self.mode === 'day') {
-          start = moment(self.position).hour(self._dayStart.h).minute(self._dayStart.m).second(0)
-          stop = moment(self.position).hour(self._dayEnd.h).minute(self._dayEnd.m).second(59)
-        } else if (self.mode === 'month') {
-          start = moment(self.position).date(1).hour(self._dayStart.h).minute(self._dayStart.m).second(0)
-          stop = moment(self.position).weekday(moment(self.position).daysInMonth()).hour(self._dayEnd.h).minute(self._dayEnd.m).second(59)
-        }
-        return e.start.isBetween(start, stop) || e.end.isBetween(start, stop)
-      })
-    }
-
-    function addMissingDays (sortedEvents) {
-      sortedEvents = sortedEvents || {}
-      var startingDay = moment(self.position).weekday(0).dayOfYear()
-      _.times(7, function (i) {
-        if (!sortedEvents[ startingDay + i ]) {
-          sortedEvents[ startingDay + i ] = []
-        }
-      })
-    }
-
-    function addMissingEntities (sortedEvents) {
-      sortedEvents = sortedEvents || {}
-      _.each(self.entities, function (e) {
-        if (!sortedEvents[ e ]) {
-          sortedEvents[ e ] = []
-        }
-      })
-    }
-
-    function parseTime (h) {
-      if (h >= 24) {
-        return { h: 23, m: 59 }
-      }
-      return { h: h, m: 0 }
-    }
-
     function init () {
       self.zoom = parseInt(self.zoom)
 
@@ -186,6 +87,64 @@
       }
     }
 
+    init()
+
+    function split (event) {
+      // Event starts and ends the same day
+      event = angular.copy(event)
+
+      var start = moment(event.start).hour(self._dayStart.h).minute(self._dayStart.m).second(0)
+      var stop = moment(event.end).hour(self._dayEnd.h).minute(self._dayEnd.m).second(59)
+      if (event.start.dayOfYear() === event.end.dayOfYear()) {
+        // reset the boundaries if they exceed the one fixed.
+        // And trim events that are entirely out of the boundaries
+        if (event.start.isBefore(start)) {
+          if (event.end.isBefore(start)) {
+            return []
+          }
+          event.start = start
+        }
+        if (event.end.isAfter(stop)) {
+          if (event.start.isAfter(stop)) {
+            return []
+          }
+          event.end = stop
+        }
+        return [ event ]
+      }
+      // Event is on several days.
+      // Build a first event that ends at the end of the first day
+      var first_event = angular.copy(event)
+      first_event.end = moment(event.start).hour(self._dayEnd.h).minute(self._dayEnd.m).second(59)
+      if (event.start.isBefore(start)) {
+        first_event.start = start
+      }
+
+      var startNextDay = moment(event.start).add(1, 'day').hour(self._dayStart.h).minute(self._dayStart.m)
+      var endThisDay = moment(event.start).hour(self._dayEnd.h).minute(self._dayEnd.m)
+      if (event.end.isBefore(startNextDay)) {
+        if (first_event.start.isAfter(endThisDay)) {
+          return []
+        }
+        // Event finishes before start hour next day. No need to create another one
+        return [ first_event ]
+      }
+      first_event.continuedAfter = true
+
+      // Build a second event that starts at the beginning of the following days.
+      // This event may end several days later. Recursion will handle it
+      var second_event = angular.copy(event)
+      second_event.start = moment(event.start).add(1, 'day').hour(self._dayStart.h).minute(self._dayStart.m)
+      second_event.continuedBefore = true
+
+      if (event.start.isAfter(endThisDay)) {
+        // If the first event starts after curfew, don't add it
+        return split(second_event)
+      }
+
+      return [ first_event ].concat(split(second_event))
+    }
+
     function splitByWeeks (event) {
       if (event.start.isAfter(event.end)) {
         var st = event.start
@@ -227,6 +186,49 @@
       return [ firstEvent ].concat(splitByWeeks(secondEvent))
     }
 
+    function filter (events) { // remove event not in range (month, week, day)
+      return _.filter(events, function (e) {
+        var start, stop
+        if (self.mode === 'week') {
+          start = moment(self.position).weekday(0).hour(self._dayStart.h).minute(self._dayStart.m).second(0)
+          stop = moment(self.position).weekday(7).hour(self._dayEnd.h).minute(self._dayEnd.m).second(59)
+        } else if (self.mode === 'day') {
+          start = moment(self.position).hour(self._dayStart.h).minute(self._dayStart.m).second(0)
+          stop = moment(self.position).hour(self._dayEnd.h).minute(self._dayEnd.m).second(59)
+        } else if (self.mode === 'month') {
+          start = moment(self.position).date(1).hour(self._dayStart.h).minute(self._dayStart.m).second(0)
+          stop = moment(self.position).weekday(moment(self.position).daysInMonth()).hour(self._dayEnd.h).minute(self._dayEnd.m).second(59)
+        }
+        return e.start.isBetween(start, stop) || e.end.isBetween(start, stop)
+      })
+    }
+
+    function addMissingDays (sortedEvents) {
+      sortedEvents = sortedEvents || {}
+      var startingDay = moment(self.position).weekday(0).dayOfYear()
+      _.times(7, function (i) {
+        if (!sortedEvents[ startingDay + i ]) {
+          sortedEvents[ startingDay + i ] = []
+        }
+      })
+    }
+
+    function addMissingEntities (sortedEvents) {
+      sortedEvents = sortedEvents || {}
+      _.each(self.entities, function (e) {
+        if (!sortedEvents[ e ]) {
+          sortedEvents[ e ] = []
+        }
+      })
+    }
+
+    function parseTime (h) {
+      if (h >= 24) {
+        return { h: 23, m: 59 }
+      }
+      return { h: h, m: 0 }
+    }
+
     function keys (sortedEvents) {
       if (self.mode === 'week') {
         return Object.keys(sortedEvents)
@@ -238,7 +240,6 @@
     function getEvents (key) {
       return self.sortedEvents[ key ]
     }
-    init()
 
     $scope.$watchCollection(function () {
       return [ self.events, self.entities, self.position, self.mode, self.dayStart, self.dayEnd, self.zoom ]
