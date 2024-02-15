a3d9a45f949a9347dd6a20981a6cde752ea6edc8
Abdelhakim Bellahdid <abdelhakim.bellahdid@90tech.fr>
2024-02-15 14:33:49 +0100

    use intersection observer

--- src/directives/drag-drop/drag-drop.directive.js
+++ src/directives/drag-drop/drag-drop.directive.js
@@ -13,83 +13,118 @@
           zlDrop: '&'
         },
         link: function (scope, element) {
-          var dragImage  = document.createElement('div')
+          const options = {
+            threshold: 0.5
+          }
+          let seen = false
+          var dragImage = document.createElement('div')
           var el = element[0]
-          if (el.attributes['zl-drag']) {
-            el.draggable = true
+          const callback = (entries, observer) => {
+            if (entries[0].isIntersecting && !seen) {
+              seen = true
+              if (el.attributes['zl-drag']) {
+                el.draggable = true
 
-            el.addEventListener(
-              'dragstart',
-              function (e) {
-                dragImage.style.height = el.clientHeight + 'px'
-                dragImage.style.zIndex = '1000'
-                dragImage.style.position = 'relative'
-                dragImage.style.left = '-10000px'
-                dragImage.style.bottom = '-10000px;'
-                dragImage.innerHTML = el.innerHTML
-                if (el.children.length > 1) {
-                  dragImage.children[0].style.float = 'left'
-                  dragImage.style.backgroundColor = el.children[1].style.backgroundColor
-                  dragImage.style.width = el.clientWidth + 'px'
-                } else {
-                  dragImage.style.width = Math.min(el.clientWidth, 200) + 'px'
-                  dragImage.style.backgroundColor = el.children[0].style.backgroundColor
-                }
-                e.dataTransfer.effectAllowed = 'move'
+                el.addEventListener(
+                  'dragstart',
+                  dragStartEvent,
+                  false
+                )
+              }
+              if (el.attributes['zl-drop']) {
+                el.addEventListener(
+                  'drop',
+                  dropEvent,
+                  false
+                )
 
-                document.body.appendChild(dragImage)
+                el.addEventListener(
+                  'dragenter',
+                  dragEnterEvent,
+                  false
+                )
 
-                // remove translucent drag if present
-                if (e.dataTransfer.setDragImage) {
-                  e.dataTransfer.setDragImage(dragImage,0,0)
-                }
-                // Remove tooltips on drag
-                _.each(document.querySelectorAll('.planning-event-tooltip'), function (elt) {
-                  elt.remove()
-                })
-                e.dataTransfer.setData('Text', JSON.stringify(scope.dragDropCtrl.zlDrag))
-                this.classList.add('drag')
-                return false
-              },
-              { useCapture: false, passive: true }
-            )
-
+                el.addEventListener(
+                  'dragleave',
+                  dragLeaveEvent,
+                  false
+                )
+              }
+            } else if (!entries[0].isIntersecting && seen) {
+              el.removeEventListener(
+                'dragstart',
+                dragStartEvent,
+                false
+              )
+              el.removeEventListener(
+                'drop',
+                dropEvent,
+                false
+              )
+              el.removeEventListener(
+                'dragenter',
+                dragEnterEvent,
+                false
+              )
+              el.removeEventListener(
+                'dragleave',
+                dragLeaveEvent,
+                false
+              )
+              seen = false
+            }
           }
-          if (el.attributes['zl-drop']) {
-            el.addEventListener(
-              'drop',
-              function (e) {
-                scope.$apply(function () {
-                  scope.dragDropCtrl.zlDrop({ $data: JSON.parse(e.dataTransfer.getData('Text')), $event: e })
-                })
-                e.target.classList.remove('over')
-                this.classList.remove('over')
-                e.stopPropagation()
-                if (e.stopPropagation) e.stopPropagation()
-                return false
-              },
-              { useCapture: false, passive: true }
-            )
+          const observer = new IntersectionObserver(callback, options)
+          observer.observe(element[0])
 
-            el.addEventListener(
-              'dragenter',
-              function (e) {
-                e.target.classList.add('over')
-                return false
-              },
-              { useCapture: false, passive: true }
-            )
+          function dragStartEvent (e) {
+            dragImage.style.height = el.clientHeight + 'px'
+            dragImage.style.zIndex = '1000'
+            dragImage.style.position = 'relative'
+            dragImage.style.left = '-10000px'
+            dragImage.style.bottom = '-10000px;'
+            dragImage.innerHTML = el.innerHTML
+            if (el.children.length > 1) {
+              dragImage.children[0].style.float = 'left'
+              dragImage.style.backgroundColor = el.children[1].style.backgroundColor
+              dragImage.style.width = el.clientWidth + 'px'
+            } else {
+              dragImage.style.width = Math.min(el.clientWidth, 200) + 'px'
+              dragImage.style.backgroundColor = el.children[0].style.backgroundColor
+            }
+            e.dataTransfer.effectAllowed = 'move'
 
-            el.addEventListener(
-              'dragleave',
-              function (e) {
-                e.target.classList.remove('over')
-                return false
-              },
-              { useCapture: false, passive: true }
-            )
+            document.body.appendChild(dragImage)
+
+            // remove translucent drag if present
+            if (e.dataTransfer.setDragImage) {
+              e.dataTransfer.setDragImage(dragImage, 0, 0)
+            }
+            // Remove tooltips on drag
+            _.each(document.querySelectorAll('.planning-event-tooltip'), function (elt) {
+              elt.remove()
+            })
+            e.dataTransfer.setData('Text', JSON.stringify(scope.dragDropCtrl.zlDrag))
+            this.classList.add('drag')
+            return false
           }
-
+          function dropEvent (e) {
+            scope.$apply(function () {
+              scope.dragDropCtrl.zlDrop({ $data: JSON.parse(e.dataTransfer.getData('Text')), $event: e })
+            })
+            e.target.classList.remove('over')
+            this.classList.remove('over')
+            e.stopPropagation()
+            return false
+          }
+          function dragEnterEvent (e) {
+            e.target.classList.add('over')
+            return false
+          }
+          function dragLeaveEvent (e) {
+            e.target.classList.remove('over')
+            return false
+          }
         }
       }
     })
